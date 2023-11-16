8999790e7c9e9971bfeeab0d79ef4e772695ec55
Alex Garrett-Smith <alex@codecourse.com>
2023-11-16 10:54:12 +0000

    Episode 05

--- app/Http/Controllers/FileStoreController.php
+++ app/Http/Controllers/FileStoreController.php
@@ -3,11 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Str;
+use Pion\Laravel\ChunkUpload\Handler\ContentRangeUploadHandler;
+use Pion\Laravel\ChunkUpload\Receiver\FileReceiver;
 
 class FileStoreController extends Controller
 {
-    public function __invoke()
+    public function __invoke(Request $request)
     {
-        //
+        $receiver = new FileReceiver(
+            UploadedFile::fake()->createWithContent('file', $request->getContent()),
+            $request,
+            ContentRangeUploadHandler::class
+        );
+
+        $save = $receiver->receive();
+
+        if ($save->isFinished()) {
+            $this->storeFile($request, $save->getFile());
+        }
+
+        $save->handler();
+    }
+
+    protected function storeFile(Request $request, UploadedFile $file)
+    {
+        $request->user()->files()->create([
+            'file_path' => $file->storeAs('videos', Str::uuid(), 'public')
+        ]);
     }
 }
--- app/Models/File.php
+++ app/Models/File.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class File extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'file_path'
+    ];
 }
