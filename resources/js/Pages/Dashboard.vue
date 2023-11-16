d508205cf804a30c31357088f65fd93f6bd09636
Alex Garrett-Smith <alex@codecourse.com>
2023-11-16 10:33:59 +0000

    Episode 03

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,10 +1,28 @@
 <script setup>
+import PrimaryButton from '@/Components/PrimaryButton.vue';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
+import { reactive, ref } from 'vue'
 
 defineProps({
     files: Array
 })
+
+const file = ref(null)
+
+const state = reactive({
+    file: null
+})
+
+const submit = () => {
+    state.file = file.value.files[0]
+
+    if (!state.file) {
+        return
+    }
+
+    console.log('start upload')
+}
 </script>
 
 <template>
@@ -16,10 +34,20 @@ defineProps({
         </template>
 
         <div class="py-12">
-            <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
+            <div class="max-w-3xl mx-auto sm:px-6 lg:px-8 space-y-3">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <form class="p-6 text-gray-900" v-on:submit.prevent="submit">
+                        <div class="flex items-center">
+                            <input type="file" name="file" ref="file" class="flex-grow">
+                            <PrimaryButton>
+                                Upload
+                            </PrimaryButton>
+                        </div>
+                    </form>
+                </div>
+
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">
-
                         <template v-if="files.length">
                             <div v-for="file in files" :key="file.id">
                                 {{ file.file_path }}
@@ -28,7 +56,6 @@ defineProps({
                         <template v-else>
                             No files yet.
                         </template>
-
                     </div>
                 </div>
             </div>
